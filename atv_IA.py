88e1f33f76a5aefd31beddff8f6a1c1bfb1c2127
Giordano <giordanopalmezano@gmail.com>
2024-03-01 20:38:41 -0300

    Add Atividade

---
+++ atv_IA.py
@@ -0,0 +1,47 @@
+filmes = ["filme1", "filme2"]
+series = ["serie1", "serie2"]
+
+def catalogo():
+    print("""\n-- Escolha qual catalogo deseja visualizar:\n
+          1 - Filmes
+          2 - Series\n
+          0 - Sair """)
+    
+    escolha = int(input("--> "))
+    
+    if escolha == 1:
+        print("\n-- Catalogo de filmes Disponiveis --\n")
+    
+        for filme in filmes:
+            print("- ", filme)
+    
+        print('\n')
+    elif escolha == 2:
+        print("\n-- Catalogo de series Disponiveis --\n")
+    
+        for serie in series:
+            print("- ", serie)
+        
+        print('\n')
+    elif escolha == 0:
+        return
+
+def main():
+    
+    while True:
+        print("""\n-- Bem vindo! --
+        \n-- Sistema Simplificado de Streaming de Video --\n
+        Opções disponiveis:\n
+        1 - Visulizar catalogo de filmes e séries
+        0 - Finalizar Programa\n
+        Digite o número da opção que deseja realizar:\n""")
+        
+        escolha = int(input("--> "))
+        
+        if escolha == 1:
+            catalogo()
+        elif escolha == 0:
+            break
+        
+    
+main()
